7986e460397d1c5aeee8a297dc629feda4385594
1391020381 <1391020381@qq.com>
2022-09-11 17:14:56 +0800

    基本类型 对象类型接口  数组 函数 断言 type class 泛型与函数 数组 接口 class 元组 枚举

--- src/views/ts-advanced.vue
+++ src/views/ts-advanced.vue
@@ -1,5 +1,36 @@
 <template>
   <div>ts-advanced</div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// 类型别名
+// 字符串字面量类型
+type Name = string;
+type NameResolver = () => string;
+type NameOrResolver = Name | NameResolver;
+function getName(n: NameOrResolver): Name {
+  if (typeof n === "string") {
+    return n;
+  } else {
+    return n();
+  }
+}
+type EventNames = "click" | "scroll" | "mousemove";
+function handleEvent(ele: Element, event: EventNames) {
+  console.log("xxxx");
+}
+// 元组
+// 数组合并了相同类型的对象,而元组 Tuple合并了不同类型的对象。
+
+// 枚举
+enum Days {
+  Sun = 7,
+  Mon,
+  Tue,
+  Wed,
+  Thu,
+  Fri,
+  Sat,
+}
+console.log(Days.Fri);
+</script>
 <style></style>
